0aae48e980c63a0e47836dc679bce57945256c55
dnieh <danielnieh@gmail.com>
2016-07-06 23:35:35 -0700

    Reworking from the ground up starting with Sass.

--- gulpfile.js
+++ gulpfile.js
@@ -1,38 +1,23 @@
+'use strict';
+
 var gulp = require('gulp');
-var sass = require('gulp-sass');
-var browserify = require('browserify');
-var babelify = require('babelify');
-var source = require('vinyl-source-stream');
+var plugins = require('gulp-load-plugins')();
 
-var scssPaths = [
-    './scss/**/*.scss',
-    './scss/bootstrap/*.scss',
-    './scss/app.scss',
-];
-
-var reactPaths = [
-    './react/**/*.jsx'
-];
+var handleError = function(error) {
+    console.error('Error:', error);
+};
 
 gulp.task('sass', function() {
-    gulp.src(scssPaths)
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./css'));
+    return gulp.src(['sass/*.scss'])
+        .pipe(plugins.plumber({ errorHandler: handleError }))
+        .pipe(plugins.sourcemaps.init())
+        .pipe(plugins.autoprefixer({ browsers: ['last 2 versions', '> 5%', 'not ie <= 8'] }))
+        .pipe(plugins.sass().on('error', handleError))
+        .pipe(plugins.sourcemaps.write('./', {
+            includeContent: false,
+            sourceRoot: '../sass/'
+        }))
+        .pipe(gulp.dest('css'));
 });
 
-gulp.task('es6-react', function() {
-    return browserify({
-        entries: './react/app.jsx',
-        extensions: ['.js', '.jsx'],
-        debug: true
-    })
-        .transform(babelify, {presets: ['es2015', 'react']})
-        .bundle()
-        .pipe(source('app.js'))
-        .pipe(gulp.dest('./js'));
-});
-
-gulp.task('watch', function() {
-    gulp.watch(scssPaths, ['sass']);
-    gulp.watch('./react/**/*.jsx', ['es6-react']);
-});
+gulp.task('default', ['sass']);
